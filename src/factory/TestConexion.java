04c125a23350a9c49ab9ec168afe26223d027967
Felipe Güell <89422098+FeliGuell@users.noreply.github.com>
2024-08-29 09:33:48 -0300

    TestConexion documentado

--- src/factory/TestConexion.java
+++ src/factory/TestConexion.java
@@ -4,12 +4,20 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class TestConexion {
+	
+    	// Método principal que se ejecuta cuando el programa comienza.
+    	// El método 'main' lanza una excepción de tipo SQLException.
 	public static void main(String[] args) throws SQLException {
+		
+		// Se crea una instancia de ConnectionFactory, la cual está configurada
+       		// para proporcionar conexiones a la base de datos usando un pool de conexiones.
 		ConnectionFactory conFactory = new ConnectionFactory();
 		Connection con = conFactory.recuperarConexion();
-		
+
 		System.out.println("Cerrando conexion");
-		
+
+		// Se cierra la conexión a la base de datos. Esto devuelve la conexión al pool si se está utilizando uno,
+        	// o simplemente libera los recursos si no es un pool.
 		con.close();
 	}
 }
